fc5090e6888fea063ee8d2243d1bcdebbe8fa40e
ZainKhan2504 <zainalikhan2504@gmail.com>
2018-03-10 17:42:58 +0500

    Print HelloWorld

--- hello_world/lib/pages/home_page.dart
+++ hello_world/lib/pages/home_page.dart
@@ -5,7 +5,14 @@ class HomePage  extends StatelessWidget{
   Widget build(BuildContext context){
     return new Material(
       color: Colors.blue,
-
+      child: new InkWell(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Hello World",style: new TextStyle(color: Colors.white, fontSize: 30.0),)
+          ],
+        )
+      ),
     );
   }
 }
